7df03b19e499bc57321bc193a25de875177a18dc
공태현 <kong3047@Khu.ac.kr>
2022-07-25 21:25:46 +0900

    Complement Routing Pages (Login & Home)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Start</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
